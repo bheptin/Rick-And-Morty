753da6704bc1cade024c1c3e193c81c4f9033e81
Brian H <bheptin@gmail.com>
2017-06-07 14:51:33 -0400

    setup

---
+++ main.js
@@ -0,0 +1,34 @@
+  function play(){
+       var audio = document.getElementById("meseeks");
+       audio.play();
+     };
+
+   function play1(){
+        var audio = document.getElementById("cromulon");
+        audio.play();
+      };
+
+    function play2(){
+         var audio = document.getElementById("Terry");
+         audio.play();
+       };
+
+   function play3(){
+        var audio = document.getElementById("LikeIt");
+        audio.play();
+      };
+
+    function play4(){
+         var audio = document.getElementById("Schwifty");
+         audio.play();
+       };
+
+     function play5(){
+          var audio = document.getElementById("Morty");
+          audio.play();
+        };
+
+      function play6(){
+           var audio = document.getElementById("DickMove");
+           audio.play();
+         };
