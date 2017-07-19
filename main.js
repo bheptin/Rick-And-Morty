23f8566b5338da3ebd0ddb9d16ab9e038f5e156e
Brian H <bheptin@gmail.com>
2017-07-19 12:29:19 -0400

    add new sounds

--- main.js
+++ main.js
@@ -3,42 +3,72 @@
        audio.play();
      };
 
-   function play1(){
-        var audio = document.getElementById("cromulon");
-        audio.play();
-      };
+  function play1(){
+      var audio = document.getElementById("cromulon");
+      audio.play();
+    };
 
-    function play2(){
-         var audio = document.getElementById("Terry");
-         audio.play();
-       };
+  function play2(){
+       var audio = document.getElementById("Terry");
+       audio.play();
+     };
 
-   function play3(){
-        var audio = document.getElementById("LikeIt");
-        audio.play();
-      };
+  function play3(){
+      var audio = document.getElementById("LikeIt");
+      audio.play();
+    };
 
-    function play4(){
-         var audio = document.getElementById("Schwifty");
-         audio.play();
-       };
+  function play4(){
+       var audio = document.getElementById("Schwifty");
+       audio.play();
+     };
 
-     function play5(){
-          var audio = document.getElementById("Morty");
-          audio.play();
-        };
+  function play5(){
+      var audio = document.getElementById("Morty");
+      audio.play();
+    };
 
-      function play6(){
-           var audio = document.getElementById("DickMove");
-           audio.play();
-         };
+  function play6(){
+       var audio = document.getElementById("DickMove");
+       audio.play();
+     };
 
-       function play7(){
-            var audio = document.getElementById("OohWee");
-            audio.play();
-          };
+  function play7(){
+      var audio = document.getElementById("OohWee");
+      audio.play();
+    };
 
-        function play8(){
-             var audio = document.getElementById("TwoBrothers");
-             audio.play();
-           };
+  function play8(){
+       var audio = document.getElementById("TwoBrothers");
+       audio.play();
+     };
+
+  function play9(){
+    var audio = document.getElementById("PickleRick");
+    audio.play();
+  };
+
+  function play10(){
+    var audio = document.getElementById("BurgerTime");
+    audio.play();
+  };
+
+  function play11(){
+    var audio = document.getElementById("MortyRaps");
+    audio.play();
+  };
+
+  function play12(){
+    var audio = document.getElementById("Disqualified");
+    audio.play();
+  };
+
+  function play13(){
+    var audio = document.getElementById("MyMan");
+    audio.play();
+  };
+
+  function play14(){
+    var audio = document.getElementById("NotCool");
+    audio.play();
+  };
